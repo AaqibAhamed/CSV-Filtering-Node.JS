bdb72168879a7149c133f4691ca7995f41eba218
AaqibGitBit <ahamedaaqib96@gmail.com>
2020-09-27 10:35:55 +0530

    before adding input strem

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 const csv = require('csv-parser');
 const fs = require('fs');
 const output = [];
-//const options = window.prompt("Filter Data on ?");
+
 
 fs.createReadStream('Person_Data.csv')
-    .pipe(csv({ headers: true }))
+    .pipe(csv({}))
     .on('data', (data) => output.push(data))
     .on('end', () => {
         console.log(output);
     });
 
+/* const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+ */
+/* fs.createReadStream('Person_Data.csv')
+    .pipe(parse({ headers: true }))
+    .pipe(csv({}))
+    .on('data', (data) => output.filter(options).push(data))
+    .on('end', readline.question('Filter Data on ? ', options => {
+            console.log(`Hey there ${options}!`);
+            readline.close();
+        });
 
-/*   fs.createReadStream('Person_Data.csv')
-  .pipe(csv({}))
-  .on('data', (data) => output.push(data))
-  .on('end', () => {
-      console.log(output);
-  }); */
+ */
